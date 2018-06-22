5e9cb8284f1d3394c51bdfd5752fbef582c0968a
DashaVolochai <d.artova@ukr.net>
2018-06-22 09:40:04 +0300

    fix

--- Timer.js
+++ Timer.js
@@ -1,9 +1,11 @@
+var shake;
 function setUp() {
     if (localStorage.getItem("timersJSON") == null) {
         localStorage.setItem("timersJSON", JSON.stringify(timers));
     }
     timers = JSON.parse(localStorage.getItem("timersJSON"));
-    alert(timers[0].description);
+    alert(timers.length);
+        
 }
 
 class Timer {
@@ -15,4 +17,13 @@ class Timer {
         this.sound = sound;
         this.description =  description;
     }
+}
+function getShake(a){
+    shake = a;
+}
+
+function add(){
+    var timer = new Timer(timers.length+1, "timer", +document.getElementById("setTime").value*60000, shake, document.getElementById("setaudio").value, document.getElementById("descrInput").value);
+    timers.push(timer);
+    localStorage.setItem("timersJSON", JSON.stringify(timers));
 }
